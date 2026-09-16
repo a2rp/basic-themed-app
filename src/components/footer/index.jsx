1519c60598cd615505a9b8e907e7862bbd475063
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-09-16 20:08:24 +0530

    Polish themed app footer and metadata

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,12 +3,20 @@ import { Styled } from "./styled";
 const Footer = () => (
     <Styled.Wrapper>
         <Styled.Main>
-            <Styled.Col>&copy; {new Date().getFullYear()} Basic Themed App</Styled.Col>
+            <Styled.Col>Copyright &copy; {new Date().getFullYear()} Basic Themed App</Styled.Col>
             <Styled.Col>
                 By{" "}
                 <a href="https://www.ashishranjan.net/" target="_blank" rel="noopener noreferrer">
                     Ashish Ranjan
                 </a>
+            </Styled.Col>
+            <Styled.Col>
+                <a href="https://github.com/a2rp" target="_blank" rel="noopener noreferrer">GitHub</a>{" "}
+                <a href="https://codepen.io/ash1198" target="_blank" rel="noopener noreferrer">CodePen</a>{" "}
+                <a href="mailto:ash.ranjan09@gmail.com">Email</a>{" "}
+                <a href="https://a2rp-donation-page.netlify.app/" target="_blank" rel="noopener noreferrer">Support</a>{" "}
+                <a href="https://buymeacoffee.com/a2rp" target="_blank" rel="noopener noreferrer">Buy Me A Coffee</a>{" "}
+                <a href="https://patreon.com/a2rp" target="_blank" rel="noopener noreferrer">Patreon</a>
             </Styled.Col>
         </Styled.Main>
     </Styled.Wrapper>
